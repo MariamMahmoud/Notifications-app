b5babb330531e6af5c5fb51b892731f552a01c48
Mariam Mahmoud <mariam.mahmoud.saad#gmail.com>
2020-07-18 16:04:32 +0200

    chore: updating config and env

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 const express = require("express");
 const app = express();
-const connectDb = require("./connection");
+const connectDb = require("./src/config/connection");
 const PORT = 8080;
-
-app.get("/users", async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-});
-
-app.post("/user", async (req, res) => {
-    user = req.body.user;
-    const user = await User.create(user);
-    res.json(user);
-});
-
-app.get("/notifications", async (req, res) => {
-    const notifications = await Notification.find();
-    res.json(notifications);
-});
-
-app.post("/notification", async (req, res) => {
-    notification = req.body.notification;
-    const notifications = await Notification.create(notification);
-    res.json(notifications);
-});
 
 app.listen(PORT, function() {
     console.log(`Listening on ${PORT}`);
--- src/config/connection.js
+++ src/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const User = require("./src/Controller/user");
+// const User = require("./src/Controller/users");
+// const notification = require("./src/Controller/notifications");
+
 const connection = "mongodb://mongo:27017/notifications-db";
 
 const connectDb = () => {
