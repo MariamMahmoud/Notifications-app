92252526e953814abe6132a043af01d33b726b5b
Mariam Mahmoud <mariam.mahmoud.saad#gmail.com>
2020-07-22 11:08:40 +0200

    fix: simplyfying notify method

--- src/Services/notify.js
+++ src/Services/notify.js
@@ -1,44 +1,16 @@
 'use strict'
 
-const baseProvider = require('../NotificationProviders/BaseProvider');
+const notificationsFactoryInstance = require('../NotificationProviders/NotificationsFactory');
 const Notifications = require('../Controllers/notifications');
 
-const _notifySMS = async(provider, notifications) => {
-    const smsNotifier = provider.getProvider('sms');
-
-    if(notifications.length > 0) {
-        for await (let smsNotification of notifications) {
-            smsNotifier.notify(smsNotification)
-        }
-    }
-};
-
-const _notifyPush = async(provider, notifications) => {
-    const pushNotifier = provider.getProvider('push');
-
-    if(notifications.length > 0) {
-        for await (let pushNotification of notifications) {
-            pushNotifier.notify(pushNotification)
-        }
-    }
-
-};
-
 module.exports = async () => {
-    const smsNotifications = await Notifications.findSMS();
-    const pushNotifications = await Notifications.findPush()
+    const notifications = await Notifications.findAllPending();
 
     try {
-        // TODO: this commented solution will create a new instance with every notification
-        // which is not memory friendly (can be solved using singleton)
-
-        // notifications.map(async notification => {
-        //     await provider.getProvider(notification.type).notify(notification);
-        // });
-
-        const provider = new baseProvider()
-        await _notifyPush(provider, smsNotifications);
-        await _notifySMS(provider,  pushNotifications);
+        notifications.forEach(async notification => {
+            let notifier = notificationsFactoryInstance.getProvider(notification.type);
+            await notifier.notify(notification);
+        });
 
         return 'Users notified successfully'
     } catch(error) {
